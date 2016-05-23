3fa2f2fd661dbb73ce8615ea7816cb82b95e5476
xuelu <289415287@qq.com>
2016-05-23 19:06:34 +0800

    添加 帖子列表和回复列表的接口地址

--- app/src/main/java/makyu/nga/constant/HttpConstant.java
+++ app/src/main/java/makyu/nga/constant/HttpConstant.java
@@ -20,6 +20,11 @@ public class HttpConstant{
     RequestQueue mQueue;
     public static final String TAG = "HttpConstant";
 
+    //帖子列表
+    public String TieziUrl = "http://bbs.nga.cn/thread.php?fid=335&lite=js";
+    //帖子回复列表
+    public String replyUrl = "http://bbs.nga.cn/read.php?tid=6726209&lite=js";
+
     public HttpConstant(Context context) {
         mQueue = Volley.newRequestQueue(context);
     }
